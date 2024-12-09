2d2afd7b6f459505e9e45f6914c91ea837e0f01d
coder-hub-013 <coderhub013@gmail.com>
2024-12-09 16:26:35 +0530

    Add Cors in file

--- index.js
+++ index.js
@@ -20,17 +20,31 @@ const http = require('http');
 const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
+// https://chat-connect-part-1.onrender.com
+// const io = new Server(server,{
+//     cors: {
+//         origin : "http://localhost:5173",
+//         credentials : true,
+//         methods : ["POST","GET"]
+//     }
+// });
+
 const io = new Server(server,{
     cors: {
-        origin : "http://localhost:5173",
+        origin : "https://chat-connect-part-1.onrender.com",
         credentials : true,
         methods : ["POST","GET"]
     }
 });
 
 //cors
+// const corsOptions = {
+//     origin : "http://localhost:5173",
+//     credentials : true,
+// }
+
 const corsOptions = {
-    origin : "http://localhost:5173",
+    origin : "https://chat-connect-part-1.onrender.com",
     credentials : true,
 }
 app.use(cors(corsOptions));
