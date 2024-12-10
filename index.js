50c417fe4bd991635e6d46f14611ed31f15601f4
coder-hub-013 <coderhub013@gmail.com>
2024-12-10 15:34:31 +0530

    Add Cors in file or main

--- index.js
+++ index.js
@@ -105,7 +105,8 @@ app.post('/router/signup',async (req,res) => {
                     // sameSite: 'none',
                     sameSite:'strict',
                     // expires : new Date(Date.now() + 3600000)
-                    expires : 3600000
+                    // expires : 3600000
+                    maxAge:3600000
                 });
                 console.log(newUser);  
                 res.status(200).json({message:"Sign up successfullly", token, user: {id:savedUser._id, email:savedUser.email,username:savedUser.username}});
@@ -139,7 +140,8 @@ app.post('/router/login',async (req,res) => {
                     secure: true, 
                     httpOnly : true, 
                     sameSite:'strict',
-                    expires : 3600000
+                    // expires : 3600000
+                    maxAge:3600000
                 });  
                 res.status(200).json({message:"Login Successfully", token, user: {id:userExist._id, email:userExist.email,username:userExist.username}});
     
