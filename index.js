a56c957970005d5eb874e15fa08d1fd3346eaf85
coder-hub-013 <coderhub013@gmail.com>
2024-12-11 16:06:45 +0530

    Add client with build

--- index.js
+++ index.js
@@ -163,6 +163,8 @@ app.post('/router/login',async (req,res) => {
 //logout
 app.post("/router/logout",(req,res) => {
     console.log("/router/logout" ,"I am triggered");
+    const cook = req.cookies;
+    console.log(cook);
     const removedCookie = res.clearCookie('jwt');
     console.log("/router/logout",removedCookie);
     if(removedCookie) {
